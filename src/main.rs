e471bc245f3b1b9069aabed6450a0ead7a45f906
Jisang Yun <journeyofbabo@gmail.com>
2023-12-01 01:34:32 +0900

    basic bmi clac

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,67 @@
-// use std::fmt;
 use std::io;
-// 키와 몸무게 데이터를 가지는 구조체 --- (*1)
-struct Body {
-    name: String,
+
+struct Body{
+    name : String,
     weight: f64,
     height: f64,
 }
 
-impl Body {
-    fn new(name: String, weight: f64, height: f64) -> Body {
-        Body {
+impl Body{
+    fn new(weight:f64, height:f64,name: String) -> Body{
+        Body { 
             name,
             weight,
             height,
-        }
+            }
     }
-    // BMI를 계산하는 함수 --- (*4)
-    fn calc_bmi(&self) -> f64 {
+
+    fn calc_bmi(&self) -> f64{
         let h = self.height / 100.0;
         self.weight / h.powf(2.0)
     }
-    fn show(&self) {
-        println!("The {}'s BMI is {:.1}", self.name, self.calc_bmi());
+
+    fn show(&self){
+        println!("{}'s BMI is {:.2}",self.name,self.calc_bmi());
     }
 }
 
-// fn input_num(prompt: &str) -> f32 { //나중에 러스트 제너릭? 으로 형 변환 알아보기
-//     println!("{}", prompt);
-//     let mut input = String::new();
-//     io::stdin().read_line(&mut input).expect("Failed to read line");
-//     input.trim().parse().expect("Please input a valid number") //return f32 number
-// }
-
-fn main() {
-    // 구조체 초기화 --- (*2)
-    println!("Type your name: ");
-    let mut name = String::new();
-    io::stdin().read_line(&mut name)
-        .expect("Faild to read line");
-    let name = name.trim().to_string();
-
-    let hong = Body::new(name ,80.0, 165.0);
-    hong.show();
-
-
-    //이름, 성별, 나이, 키, 몸무게 입력하면 맞춰서 정보제공
-    //회원번호 입력해서 정보 저장
-    println!("Welcome to Health Indicator");
-    loop{
-            println!("Type your name pls: ");
-            
-        
-
-    }
-
+fn input_float(ino: &str) -> f64 {
+    println!("{}",ino);
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("Failed to read line");
+    s.trim().parse().expect("Please input valid number")
 }
 
+fn input_string(ino: &str) -> String {
+    println!("{}",ino);
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("Failed to read line");
+    s.trim().to_string()
+}
+
+
+fn main(){
+
+    println!("Write you name, weight, height");
+    
+    let n = input_string("Write you name: ");
+    let w = input_float("Write weight: ");
+    let h = input_float("Write height: ");
+    let b = Body::new(w,h,n);
+    b.show();
+
+    let hong = Body{
+        name: "hong".to_string(),
+        weight: 80.0,
+        height:165.0,
+    };
+    let lim = Body{
+        name: "lim".to_string(),
+        weight:65.0,
+        height:170.0,
+    };
+
+    println!("홍길동={:.2}",hong.calc_bmi());
+    println!("임꺽정={:.2}",lim.calc_bmi());
+
+}
